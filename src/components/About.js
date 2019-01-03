18ad554651420a1c9d7598011e69326bfe0c4cfc
crispe1586 <crispe1586@gmail.com>
2019-01-03 19:52:20 +0100

    About section

--- src/components/About.js
+++ src/components/About.js
@@ -8,23 +8,22 @@ class About extends Component {
       }
       return(
         <div id="about" className="Section">
-                <span className="Title" style={ titleStyles }>About</span>
-                <div className="Body">
-                    <div className="Description">
-                        <p>
-                            I am Cristina, a web enthusiast who started coding in 2011 as a full-stack developer, and later decided to focus exclusively in front-end technologies.
-                        </p>
-                        <p>
-                            Given my full-stack dev past, I have got a diverse set of skills, ranging from PHP/CakePHP, MySQL, AWS and CCNA, all the way down to HTML, CSS/SCSS, JavaScript, VueJS, ReactJS, and Redux.
-                        </p>
-                        <p>
-                            Take me as a self learner who is continuously seeking out for new tools and tech trends in order to be up to date as the web evolves.
-                        </p>
-                    </div>
-                    <div className="Photo">
-                        <img src="/me.jpg" alt="My profile picture" />
-                    </div>
-                </div>
+          <span className="Title" style={ titleStyles }>About</span>
+          <div className="Body">
+            <div className="Photo">
+              <img src="/me.jpg" alt="Me" />
+            </div>
+            <div className="Description">
+              <div>
+                <span className="Subtitle">Who I am</span>
+                A web enthusiast who started coding in 2011 as a full-stack developer, and later decided to focus in front-end technologies. A self learner continuously seeking for new tech trends in order to be up to date as the web evolves.
+              </div>
+              <div>
+                <span className="Subtitle">My work</span>
+                Given my full-stack dev past, I have got a diverse set of skills, ranging from <u>PHP</u>, CakePHP, Python, C, <u>MySQL</u>, <u>AWS</u>, Heroku, and CCNA, all the way down to HTML, Twig, <u>CSS/SCSS</u>, <u>JavaScript</u>, VueJS, <u>ReactJS</u>, and <u>Redux</u>.
+              </div>
+            </div>
+          </div>
         </div>
     );
     }
