45fda02387ac86f64dea5674021730ec0ac69932
crispe1586 <crispe1586@gmail.com>
2019-01-04 23:45:27 +0100

    Optimize navigation styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import { HashLink as Link } from 'react-router-hash-link'
+import styleVars from '../assets/scss/_vars.scss'
 
 class Nav extends React.Component {
 
   render () {
     return (
       <div className="Nav">
-        <Link smooth to="/#about" className="NavLink">
+        <Link
+          smooth
+          to="/#about"
+          className="NavLink"
+          style={{ color: styleVars.colorBlue }}>
           <span className="NavItem">About</span>
         </Link>
-        <Link smooth to="/#stack" className="NavLink">
+        <Link
+          smooth
+          to="/#stack"
+          className="NavLink"
+          style={{ color: styleVars.colorGreen }}>
           <span className="NavItem">Stack</span>
         </Link>
-        <Link smooth to="/#contact" className="NavLink">
+        <Link
+          smooth to="/#contact"
+          className="NavLink"
+          style={{ color: styleVars.colorPink }}>
           <span className="NavItem">Contact</span>
         </Link>
       </div>
